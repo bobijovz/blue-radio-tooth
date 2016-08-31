f1c358d73accaadce32ae9254f3664d4231102f1
Nico Villegas <john@tagcash.com>
2016-08-31 17:00:22 +0800

    PAIRING

--- app/src/main/java/com/itseasyright/app/blueradiotooth/MainActivity.java
+++ app/src/main/java/com/itseasyright/app/blueradiotooth/MainActivity.java
@@ -1,39 +1,133 @@
 package com.itseasyright.app.blueradiotooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 245;
+    BluetoothAdapter bluetoothAdapter;
+    ImageView ivScan;
+    ListView lvDevices;
+    ArrayAdapter adapter;
+    BluetoothDevice device;
+    Set<BluetoothDevice> pairedDevices;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivScan = (ImageView) findViewById(R.id.toggle_mic);
+        lvDevices = (ListView) findViewById(R.id.lv_devices);
         initialize();
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_bluetooth_devices, R.id.tv_bt);
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                adapter.add(device.getName() + "\n" + device.getAddress() + " (PAIRED)");
+            }
+        }
+        lvDevices.setAdapter(adapter);
+
+        ivScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId())
+                {
+                    case R.id.toggle_mic:
+                        bluetoothAdapter.startDiscovery();
+                        // Register the BroadcastReceiver
+                        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                        registerReceiver(mReceiver, filter);
+                        break;
+                }
+            }
+        });
+
+        lvDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                unregisterReceiver(mReceiver);
+                try {
+                    Log.d("pairDevice()", "Start Pairing...");
+                    Method m = device.getClass().getMethod("createBond", (Class[]) null);
+                    m.invoke(device, (Object[]) null);
+                    Log.d("pairDevice()", "Pairing finished.");
+                    adapter.clear();
+                    pairedDevices = bluetoothAdapter.getBondedDevices();
+                    if (pairedDevices.size() > 0) {
+                        // Loop through paired devices
+                        for (BluetoothDevice device : pairedDevices) {
+                            // Add the name and address to an array adapter to show in a ListView
+                            adapter.add(device.getName() + "\n" + device.getAddress() + " (PAIRED)");
+                        }
+                    }
+                    lvDevices.setAdapter(adapter);
+                } catch (Exception e) {
+//                    Log.d("pairDevice()", e.getMessage());
+                }
+
+            }
+        });
     }
 
-    public void initialize(){
+    public void initialize() {
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!mBluetoothAdapter.isEnabled()) {
-            Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(bluetoothIntent, REQUEST_ENABLE_BT);
+            Intent enableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            bluetoothDiscoverable();
         } else {
-
+            bluetoothDiscoverable();
         }
     }
+
+    public void bluetoothDiscoverable() {
+        Intent discoverableIntent = new Intent(
+                BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(
+                BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                adapter.add(device.getName() + "\n" + device.getAddress() + " (UNPAIRED)");
+            }
+        }
+    };
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
-
-        } else {
-            this.finish();
-        }
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
     }
+
 }
